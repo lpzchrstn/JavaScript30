ebdd539e75f74cb7eddfb79bd8b5083635d9b446
Christian Gabriel Lopez <christian.gabriel.lopez@codeandtheory.com>
2022-04-26 16:42:12 +0800

    Cleanup

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,95 +5,92 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then( localMediaStream => {
-            video.srcObject = localMediaStream;
-            video.play();
-        })
-        .catch(err => {
-            console.error(`OH NO!!!`, err);
-        });
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
 }
 
 function paintToCanvas() {
-    const {videoWidth: width, videoHeight: height} = video;
-    canvas.width = width;
-    canvas.height = height;
+  const { videoWidth: width, videoHeight: height } = video;
+  canvas.width = width;
+  canvas.height = height;
 
-    return setInterval(() => {
-        ctx.drawImage( video, 0, 0, width, height );
-        let pixels = ctx.getImageData(0, 0, width, height);
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
 
-        //This is to change the effect shown randomly per paint on canvas
-        const effects = ['redEffect', 'rgbSplit', 'greenScreen'];
-        const effectIndex = Math.floor( Math.random() * 3 );
+    // This is to change the effect shown randomly per paint on canvas
+    const effects = ['redEffect', 'rgbSplit', 'greenScreen'];
+    const effectIndex = Math.floor(Math.random() * 3);
 
-        pixels = window[effects[effectIndex]](pixels);
-        ctx.putImageData(pixels, 0, 0);
-    }, 10);
+    pixels = window[effects[effectIndex]](pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 10);
 }
 
 function takePhoto() {
-    snap.currentTime = 0;
-    snap.play();
+  snap.currentTime = 0;
+  snap.play();
 
-    const data = canvas.toDataURL( 'image/jpeg' );
-    const link = document.createElement( 'a' );
-    link.href = data;
-    link.setAttribute( "download", "handsome" );
-    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-    strip.insertBefore( link, strip.firstChild );
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src='${data}' alt='Handsome Man' />`;
+  strip.insertBefore(link, strip.firstChild);
 }
 
-function redEffect( pixels ) {
-    for( let i = 0; i < pixels.data.length; i+=4 ) {
-        pixels.data[i + 0] += 155;
-        pixels.data[i + 1] += 0;
-        pixels.data[i + 2] += 0;
-    }
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] += 155;
+    pixels.data[i + 1] += 0;
+    pixels.data[i + 2] += 0;
+  }
 
-    return pixels;
-};
+  return pixels;
+}
 
-function rgbSplit( pixels ) {
-    for( let i = 0; i < pixels.data.length; i+=4 ) {
-        pixels.data[i - 150] = pixels.data[i + 0];
-        pixels.data[i + 100] = pixels.data[i + 1];
-        pixels.data[i - 150] = pixels.data[i + 2];
-    }
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 100] = pixels.data[i + 1];
+    pixels.data[i - 150] = pixels.data[i + 2];
+  }
 
-    return pixels;
+  return pixels;
 }
 
 function greenScreen(pixels) {
-    const levels = {};
-  
-    document.querySelectorAll('.rgb input').forEach((input) => {
-      levels[input.name] = input.value;
-    });
+  const levels = {};
 
-    for (i = 0; i < pixels.data.length; i = i + 4) {
-      red = pixels.data[i + 0];
-      green = pixels.data[i + 1];
-      blue = pixels.data[i + 2];
-      alpha = pixels.data[i + 3];
-  
-      if (red >= levels.rmin
-        && green >= levels.gmin
-        && blue >= levels.bmin
-        && red <= levels.rmax
-        && green <= levels.gmax
-        && blue <= levels.bmax) {
-            
-        pixels.data[i + 0] = Math.floor(Math.random() * 256);
-        pixels.data[i + 1] = Math.floor(Math.random() * 256);
-        pixels.data[i + 2] = Math.floor(Math.random() * 256);
-        pixels.data[i + 3] = 255;
-      }
+  [...document.querySelectorAll('.rgb input')].forEach(input => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    const red = pixels.data[i + 0];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+
+    const redInMinMax = red >= levels.rmin && red <= levels.rmax;
+    const blueInMinMax = blue >= levels.bmin && blue <= levels.bmax;
+    const greenInMinMax = green >= levels.gmin && green <= levels.gmax;
+
+    if (redInMinMax && blueInMinMax && greenInMinMax) {
+      pixels.data[i + 0] = Math.floor(Math.random() * 256);
+      pixels.data[i + 1] = Math.floor(Math.random() * 256);
+      pixels.data[i + 2] = Math.floor(Math.random() * 256);
+      pixels.data[i + 3] = 255;
     }
-  
-    return pixels;
   }
+
+  return pixels;
+}
 
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
